2b5f567d55a9834b536498a1893907c64d194b64
Vvek <41186164+Vv3kS@users.noreply.github.com>
2024-02-12 01:03:15 +0530

    Add files via upload

---
+++ HomeComp.js
@@ -0,0 +1,8 @@
+export const HomeComp = ()=>{
+    return(
+        <div className="container-fluid custom-bg">
+      
+            <h1 style={{fontFamily:"Antic Didone", color:"black", backgroundColor:"white"}}>WELLCOME TO EVENT BOOKING WEB APPLICATION</h1>            
+        </div>
+    )
+}
